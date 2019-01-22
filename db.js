5124941de1d0e0e6cf9074fec09d4bd76573513b
William Wrigley <williamawrigley@gmail.com>
2019-01-22 15:22:42 +1300

    change sequelize params

--- db.js
+++ db.js
@@ -1,9 +1,12 @@
 const Sequelize = require('sequelize');
 const config = require('./config');
 
-const db = new Sequelize('AppForApps', 'postgres', 'root', {
-    host : config.DATABASE_URL,
+console.log("Connection String", config.DATABASE_URL);
+
+const db = new Sequelize('dci52fapfflq1', 'wpzgzqdwyntlkb', '91ad8617c811af48f163cb003ab77f92339dc6041ef557dbd20be51ececd56e4', {
     dialect : 'postgres'
 });
+
+db.createSchema("AppForApps");
 
 module.exports = db;
