add43ac66266fa6376e2c82267406ce290994cdb
William Wrigley <williamawrigley@gmail.com>
2019-01-22 16:16:33 +1300

    pls work

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use("/api/comment", Comments);
 app.use("/api/idea", Ideas);
 
 if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('/client/dist'));
+  app.use(express.static('client/dist'));
   app.get("*", () => (req, res) =>{
     res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
   })
